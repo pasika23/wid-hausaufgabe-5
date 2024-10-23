3a5e33131a9a0c4b8792821a4cf12c611833f371
pasika23 <pascal.kalbermatten@students.fhnw.ch>
2024-10-24 01:03:50 +0200

    MUI-Import und keine Anpassungen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import {Typography,Button} from "@mui/material";
+
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return(
+    <div>
+      <Typography variant="h3">Hello World</Typography>
+      <Button variant="outlined">Hier klicken</Button>
+    </div>
+  );
 }
  
 export default App;
